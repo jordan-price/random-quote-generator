9ddb41799bb0c00435833eccbc5decb6747c6490
Jordan Price <jordan@solid.city>
2016-07-24 17:09:19 -0600

    Bug Fixes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,65 @@
+//Create an array of javaScript objects to hold the data for the quotes
+	var quotes = [
+
+		{
+			quote: "You mustn't be afraid to dream a little bigger, darling.",
+			source: "Eames (Tom Hardy)",
+			citation: "Inception",
+			year: "2010",
+			tag: "Movie",
+		},
+		{
+			quote: "Genius is one percent inspiration and ninety-nine percent perspiration.",
+			source: "Thomas Alva Edison",
+			citation: "Harper's Monthly Magazine",
+			year: "1902",
+			tag: "Innovator",
+		},
+		{
+			quote: "Stay hungry. Stay foolish",
+			source: "Steve Jobs",
+			citation: "Stanford Univerisy Commencement Speech",
+			year: "2005",
+			tag: "Innovator",
+		},
+		{
+			quote: "Imagination is more important than knowledge.",
+			source: "Albert Einstein",
+			citation: "What Life Means to Einstein",
+			year: "1929",
+			tag: "Physicist",
+		},
+		{
+			quote: "No! Try not. Do, or do not. There is no try.",
+			source: "Yoda",
+			citation: "Star Wars Episode V: The Empire Strikes Back",
+			year: "1980",
+			tag: "Movie",
+		},
+		{
+			quote: "It always seems impossible, until it is done.",
+			source: "Nelson Mandela",
+			citation: "QI",
+			year: "2001",
+			tag: "Politician",
+		},
+		{
+			quote: "Success always demands a greater effort.",
+			source: "Winston Churchill",
+			citation: "Their Finest Hour",
+			year: "1949",
+			tag: "Politician",
+		}
+	];
+
+	var usedQuotes = [];
+
+	var colors = [
+		'firebrick', 
+		'lightgray', 
+		'lightblue', 
+		'lightgreen'
+	];
 
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
@@ -19,8 +81,6 @@ buttonClick(changeColor);
 		// returns the randomly selected quote object
 		return randomNumber;
 	}
-
-	function noRepeat() {}
 
 
 // create a function named printQuote
@@ -46,17 +106,18 @@ buttonClick(changeColor);
 		quoteHTML +='<p class="quote">' + quote + ' </p> <p class="source">' + source;
 
 		// printQuote doesn't add a <span class="citation"> for a missing citation or a <span class="year"> if the year property is missing
-		if (citation != null) {
+		if (citation !== null) {
 			quoteHTML += '<span class="citation"> ' + citation + '</span>';
-		};
-		if (year != null) {
+		}
+    
+		if (year !== null) {
 			quoteHTML += '<span class="year"> ' + year + ' </span>';		
-		};
-		// if (tag != null) {
-		// 	quoteHTML += '<span class="tag"> ' + tag + ' </span>';		
-		// };
+		}
+		if (tag !== null) {
+			quoteHTML += '<br><a href="#"><span class="tag">' + tag + ' </span></a>';		
+		}
 
-		quoteHTML += '</p>'
+		quoteHTML += '</p>';
 		    
 		// printQuote displays the final HTML string to the page. You can use the following JS snippet to accomplish that: 
 		document.getElementById('quote-box').innerHTML = quoteHTML;
@@ -81,17 +142,18 @@ buttonClick(changeColor);
 		var color = getRandomColor();
 
 		//change color
-		var bgColor = document.getElementById('change_bg')
+		var bgColor = document.getElementById('change_bg');
 		bgColor.style.background = color;
 
 		return bgColor;
-}
+		}
 
 // TODO: Refresh the quote after a set amount of time. For example, every 30 seconds, make a new quote appear. 
 	var nIntervId;
 
 	switchQuote();
 	switchBgColor();
+
 	function switchQuote () {
 		nIntervId = setInterval(printQuote, 30000);
 
